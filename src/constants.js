36269873db827359e51183af41ec20a2482e3b5d
marekwino23 <marekw1996@gmail.com>
2025-10-28 20:09:06 +0100

    setup old fitness

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,4 @@
 // export const BACKEND_URL = "http://localhost:4000";
-export const BACKEND_URL = "https://fitness-backend-tf7l.onrender.com";
+// export const BACKEND_URL = "https://fitness-backend-tf7l.onrender.com";
+export const BACKEND_URL = "https://backend-gym-old.onrender.com";
 // export const BACKEND_URL = "https://ems2024.onrender.com";
